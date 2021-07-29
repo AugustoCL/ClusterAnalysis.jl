6862ffcd21539528f42bf216f231e1c0d4fa867a
Elias Carvalho <eliascarv1998@gmail.com>
2021-07-29 14:06:48 -0300

    Adding Comments

--- src/kmeans.jl
+++ src/kmeans.jl
@@ -18,7 +18,8 @@ end
 Create the K-means cluster model and also initialize calculating the first centroids, estimating clusters and total variance.
 
 # Constructors
-```julia 
+```julia
+Kmeans(df::Matrix{T}, K::Int) where {T<:AbstractFloat} : default constructor. 
 Kmeans(df::Matrix{T}, K::Int) where {T} = Kmeans(Matrix{Float64}(df), K) : where df is an Matrix{T} where T is not a subtype of AbstractFloat.
 Kmeans(df, K::Int) : where df implements the Tables.jl interface, e.g.: DataFrame type from DataFrames.jl package.   
 ```
