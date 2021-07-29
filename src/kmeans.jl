0732eab803562bdd40314514049916ca6bb30fc9
Elias Carvalho <eliascarv1998@gmail.com>
2021-07-29 09:26:34 -0300

    Add Tables Interface

--- src/kmeans.jl
+++ src/kmeans.jl
@@ -66,11 +66,11 @@ mutable struct Kmeans{T<:AbstractFloat}
     end
 end
 
-# Kmeans(df::Matrix{T}, K::Int) where {T} = Kmeans(Matrix{Float64}(df), K)
+Kmeans(df::Matrix{T}, K::Int) where {T} = Kmeans(Matrix{Float64}(df), K)
 # Kmeans(df::DataFrame, K::Int) = Kmeans(Matrix{Float64}(df), K)
 
 function Kmeans(df, K::Int)
-    df = Tables.istable(df) ? Tables.matrix(df) : Matrix{Float64}(df) 
+    Tables.istable(df) ? (df = Tables.matrix(df)) : throw(ArgumentError("The passed df argument not implementes Tables.jl interace.")) 
     eltype(df) <: AbstractFloat || (df = Matrix{Float64}(df))
     return Kmeans(df, K)
 end
